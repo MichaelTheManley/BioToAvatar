8c2b0d5d2521da9e0369800221bf2676f271c190
Michael Manley <manleym362@gmail.com>
2024-12-11 20:07:09 +0200

    Frontend visuals complete; TODO Backend and ability to change picture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import React from 'react';
 function App() {
   // State variables for the biography and the biography input
   const [biography, setBiography] = React.useState("");
-  const [biographyInput, setBiographyInput] = React.useState(true);
+  const [biographyInput, setBiographyInput] = React.useState(false);
+  const [AIPhoto, setAIPhoto] = React.useState('https://p.potaufeu.asahi.com/1831-p/picture/27695628/89644a996fdd0cfc9e06398c64320fbe.jpg');
 
   // Function to check if the biography is valid so people can't just enter in one character/word
   function isValidBiography(bio) {
@@ -17,36 +18,52 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to MindJoy!</h1>
-        <p>
-          Ready to create your avatar? Start by entering an exciting biography below!
-        </p>
-        <textarea
-          value={biography}
-          onChange={e => 
-            {
-              setBiography(e.target.value);
-              if ((e.target.value != "") && (isValidBiography(e.target.value))) {
-                setBiographyInput(true);
-              } else {
-                setBiographyInput(false);
+        <div className ="Top">
+          <h1 className="Heading">Vocalizing <span className="AI">AI</span> Avatars</h1>
+          <h2 className="Subheading">
+            Ready to create your avatar? Start by entering an exciting biography below!
+          </h2>
+        </div>
+        <div className="Middle">
+          <div className="MiddleLeft">
+            <textarea className = "TextBox"
+              value={biography}
+              onChange={e => 
+                {
+                  setBiography(e.target.value);
+                  if ((e.target.value != "") && (isValidBiography(e.target.value))) {
+                    setBiographyInput(true);
+                  } else {
+                    setBiographyInput(false);
+                  }
+                }
               }
-            }
-          }
-          placeholder="Enter your amazing biography here..."
-          style={{width: "45%", height:"150px", fontSize: "20px", resize: "both"}}
-        />
-        {!biographyInput && (
-          <button className="App-submitFalse" disabled>
-            Submit
-          </button>
-        )}
-        {biographyInput && (
-          <button className="App-submitTrue">
-            Submit
-          </button>
-        )}
-        <img src={logo} className="App-logo" alt="logo" />
+              placeholder="Enter your amazing biography here..."
+            />
+            {!biographyInput && (
+              <button className="SubmitFalse" disabled={true}>
+                Submit
+              </button>
+            )}
+            {biographyInput && (
+              <button className="SubmitTrue" disabled={false}>
+                Submit
+              </button>
+            )}
+          </div>
+          <div className="MiddleRight">
+            <img 
+              src={AIPhoto} 
+              className="Avatar" 
+              alt="avatar" 
+            />
+            <img 
+              src={logo} 
+              className="Logo" 
+              alt="Mindjoy_Logo" 
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
